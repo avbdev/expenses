52abf067050724c2a5b6d153ec541e2767098d03
Bhardwaj Avasarala <bhardwaj@avb.dev>
2021-07-06 14:17:14 -0700

    Bootstrapping Expenses Backend environment

---
+++ Expenses/Expenses.API/Controllers/ExpensesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ExpensesController : ControllerBase
+	{
+		private readonly ILogger<ExpensesController> _logger;
+
+		public ExpensesController(ILogger<ExpensesController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IActionResult GetExpenses() 
+		{
+
+			return Ok("Expenses are empty");
+		}
+		
+	}
+}
---
+++ Expenses/Expenses.DB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Expenses.DB
+{
+	public class Class1
+	{
+	}
+}
